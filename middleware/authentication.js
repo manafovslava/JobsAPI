8510794248c9ffc88b0fb8349cbe21c85bb2965a
Viacheslav Manafov <manafovslava@gmail.com>
2023-11-10 10:08:57 +0200

    jobs api

---
+++ middleware/authentication.js
@@ -0,0 +1,28 @@
+const User = require('../models/User')
+const jwt = require('jsonwebtoken')
+const { UnauthenticatedError } = require('../errors')
+
+const auth = (req, res, next) => {
+   //check heder
+   const authHeader = req.headers.authorization
+   if(!authHeader || !authHeader.startsWith('Bearer')){
+      throw new UnauthenticatedError('Authentication invalid')
+   }
+
+   const token = authHeader.split(' ')[1]
+
+   try {
+      const payload = jwt.verify(token, process.env.JWT_SECRET)
+      // attach the user to job routes
+
+      const user = User.findById(payload.id).select('-passwoed')
+      req.user = user 
+
+      req.user = { userId: payload.userId, name:payload.name}
+      next();
+   } catch (error) {
+      throw new UnauthenticatedError('Authentication invalid')
+   }
+}
+
+module.exports = auth;
